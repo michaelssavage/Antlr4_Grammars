21a87be47d1a19547c95ca3a4a1f32e173466e16
Michael S <33690140+michaelssavage@users.noreply.github.com>
2020-11-11 22:47:46 +0000

    for loop batch file run each file

--- cal/testcal.bat
+++ cal/testcal.bat
@@ -1,12 +1,12 @@
 @echo off
-cd C:\Users\micha\cal
 echo %cd%
 CALL antlr4 cal.g4 -no-listener
 echo Generating antlr files...
 javac *.java
 echo Compiling Java files...
-:start
-set /p file="Enter file name: "
-CALL java Cal %file%
 echo.
-GOTO :start
+FOR /L %%A IN (1,1,6) DO (
+  CALL java Cal file%%A.cal
+)
+echo.
+pause
