b246d6fedebf0facdd38ec6f7fa8d6eff4ede8c2
Michael S <33690140+michaelssavage@users.noreply.github.com>
2020-11-11 22:44:21 +0000

    boolean set to private and name change

--- cal/CalErrorListener.java
+++ cal/CalErrorListener.java
@@ -3,21 +3,26 @@ import org.antlr.v4.runtime.*;
 public class CalErrorListener extends BaseErrorListener {
 
     public static final CalErrorListener INSTANCE = new CalErrorListener();
-    public static boolean REPORT_SYNTAX_ERRORS = true;
+
+    //initially called REPORT_SYNTAX_ERRORS
+    private static boolean NO_ERRORS = true;
 
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,
                             int line, int charPositionInLine,
                             String msg, RecognitionException e){
-        if (!REPORT_SYNTAX_ERRORS) {
+        if (!NO_ERRORS) {
             return;
         }
-        
         //if syntaxError is called, then we can change the boolean through this function.
         setError();
     }
-    //this function is static, so we can change the REPORT_SYNTAX_ERRORS variable.
+    //this function is static, so we can change the NO_ERRORS variable.
     public static void setError(){
-        REPORT_SYNTAX_ERRORS = false;
+        NO_ERRORS = false;
+    }
+    //get the value of the private variable
+    public static boolean getError(){
+        return NO_ERRORS;
     }
 }
